0e7bebadabaaadd755b391bcf6e5b32ed5b753e9
ihebjemmali <ihebjemmali000@gmail.com>
2024-10-29 12:10:59 +0100

    myCategory + myService ( controller and routes )

--- server/server.js
+++ server/server.js
@@ -2,17 +2,20 @@ const express = require("express");
 const cors = require("cors");
 const userRouter = require("./routes/userRoutes");
 const serviceRouter = require("./routes/serviceRoutes");
+const myCategoryRoutes = require("./routes/myCategoryRoutes");
+const myServiceRoutes = require("./routes/myServiceRoutes");
 
 const { PrismaClient } = require("@prisma/client");
 
 const app = express();
 app.use(cors());
 app.use(express.json());
-app.use(express.urlencoded({extended:true}));
+app.use(express.urlencoded({ extended: true }));
 
 app.use("/user", userRouter);
 app.use("/service", serviceRouter);
-
+app.use("/api/my-categories", myCategoryRoutes);
+app.use("/api/my-services", myServiceRoutes);
 
 const prisma = new PrismaClient();
 
